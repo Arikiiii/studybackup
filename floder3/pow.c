f35eeedd934d0573a475dfc643bfd6faddbc0016
Arikiiii <144315244+Arikiiii@users.noreply.github.com>
2023-09-07 17:25:24 +0700

    pow
    pow

---
+++ floder3/pow.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    double x,y ;
+    printf("enter base and exponent : "); scanf("%lf %lf",&x,&y);
+    printf("%.0f ^ %.0f = %.0f\n",x,y,pow(x,y));
+    return 0;
+}
