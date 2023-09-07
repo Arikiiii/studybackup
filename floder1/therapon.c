80c76f905bd011f62b6fb35153900f87a984412d
Arikiiii <144315244+Arikiiii@users.noreply.github.com>
2023-09-07 14:13:25 +0700

    cvb
    cvb

--- floder1/therapon.c
+++
@@ -1,7 +0,0 @@
-# include <stdio.h>
-int main (void)
-{
-    printf("Therapon\n166414040052\n");
-    return 0;
-    
-}
--- floder2/for_next.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (){
-    int i;
-    for (i=12;i>1;i--){
-        printf ("2*%d = %d\n",i,2*i);  
-    }
-    return 0;
-}
