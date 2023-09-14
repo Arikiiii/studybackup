c441d4badc2fffd6fe692479a45dabf13735d9c2
Arikiiii <144315244+Arikiiii@users.noreply.github.com>
2023-09-14 13:22:06 +0700

    /
    ??

--- floder3/pow.c
+++ floder3/pow.c
@@ -2,7 +2,8 @@
 #include <math.h>
 int main (){
     double x,y ;
-    printf("enter base and exponent : "); scanf("%lf %lf",&x,&y);
+    printf("enter base and exponent : ");
+    scanf("%lf %lf",&x,&y);
     printf("%.0f ^ %.0f = %.0f\n",x,y,pow(x,y));
     return 0;
 }
---
+++ folder/Fmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void myName();
+int main(){
+    myName();
+    return 0;
+}
+
+void myName()
+{
+    printf("My name is Kritsana");
+}
