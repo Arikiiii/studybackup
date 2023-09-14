8b2b6104ec385681983c84de099a6ba33ba81cb1
Arikiiii <144315244+Arikiiii@users.noreply.github.com>
2023-09-14 16:55:49 +0700

    1561

--- folder4/func4.c
+++ folder4/func4.c
@@ -4,30 +4,18 @@ float rentCAl(float rent_1,float rent_2);
 float rectParaCal(float wight,float hight);
 float triangleCal(float wight,float higt);
 int main (){
-    float radius=4;
-    float hight3=5,wight3=5;
-    float wight2=10,hight2=5;
-    float wight4_1=10,hight4_1=8;
-    float wight4_2=5,hight4_2=8;
-    float all_area;
-    all_area = circleCal(radius)+triangleCal(hight3,wight3)+rectParaCal(wight2,hight2)+
-    rentCAl(wight4_1,hight4_1)+rentCAl(wight4_2,hight4_2);
-    printf(" All area : %.2f",all_area);
+    printf(" All area : %.2f",circleCal(4)+triangleCal(5,5)+rectParaCal(10,5)+rentCAl(10,8)+rentCAl(5,8));
+    return 0;
 }
-
-//วงกลม
 float circleCal(float radius){
-    return (radius * radius)*3.14159;
+    return (radius * radius)*3.14159;//วงกลม
 }
-//สี่เหลี่ยมผืนผ้า
 float rentCAl(float rent_1,float rent_2){
-    return rent_1*rent_2;
+    return rent_1*rent_2;//สี่เหลี่ยมผืนผ้า
 }
-//สี่เหลี่ยมด้านขนาน
 float rectParaCal(float wight,float hight){
-    return wight*hight;
+    return wight*hight;//สี่เหลี่ยมด้านขนาน
 }
-//สามเหลี่ยม
 float triangleCal(float wight,float higt){
-    return (wight*higt)/2;
+    return (wight*higt)/2;//สามเหลี่ยม
 }
